da01a213d4a17d1451dfa6691aaa1674144d29e7
avrybintsev <avrybintsev@gmail.com>
2013-01-25 23:56:39 +0400

    changes in iprb.py

--- iprb.py
+++ iprb.py
@@ -8,17 +8,8 @@ k = int(sys.argv[1])
 m = int(sys.argv[2])
 n = int(sys.argv[3])
 
-def f(n):
-	if n == 0:
-		return 1
-	else:
-		return f(n-1)*n
-
 def c2(n):
-	if n <= 1:
-		return 0
-	else:
-		return f(n)/(2*f(n-2))
+	return n*(n-1)/2
 
 total = k*m + k*n + m*n + c2(k) + c2(m) + c2(n)
 answer = (k*m + k*n + 0.5*m*n + c2(k) + 0.75*c2(m))/total
